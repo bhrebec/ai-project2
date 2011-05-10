242400fb9b0ac6fa1429a1f846e680f3f8946858
Brian Hrebec <brianh32@gmail.com>
2011-05-09 23:00:19 -0500

    Might be done
    Added best data result, FileTest.java

---
+++ FileTest.java
@@ -0,0 +1,78 @@
+import java.io.*;
+/** This program verifies the correctness of form of an entry to the 2011 Pummill Relays computer 
+* programming problem, held at Missouri State University Apr. 20, 2011. The expected form of each
+* of the entered files is a file in plain text format, containing on separate lines integers 
+* representing the following information: 
+*		the number of lines in the file (including this line)
+*		an ID number of a vertex in the set
+*		an ID number of a vertex in the set
+*		an ID number of a vertex in the set
+* 			. 
+*			  .
+*			  	.
+*
+*
+* This program expects as a parameter the filename to be checked.
+*/
+class FileTest {
+
+	public static void main(String[] args) {
+		FileTest ft = new FileTest();
+		
+		try {
+			ft.readFile(args[0]);
+		} catch(ArrayIndexOutOfBoundsException e) {  // Missing the required filename parameter!
+			System.out.println("\n\tError: Must use as parameter the filename to be checked.\n");
+			System.exit(0);
+		}
+
+	}
+
+	void readFile(String file) {
+		int num_lines = 0;			//number of supposed lines
+		int act_lines = 0;			//number of actual lines
+		int current_data = 0; 		// data on current line of file
+		
+		try {
+			FileReader f = new FileReader(file);
+			BufferedReader br = new BufferedReader(f);
+			
+			String s;
+			
+			if((s = br.readLine()) != null) {			//read in number of lines including this line
+				num_lines = Integer.parseInt(s);		//turn into integer
+				act_lines++;							//increase actual lines
+				while((s = br.readLine()) != null) {
+					current_data = Integer.parseInt(s);	//turn into integer
+					act_lines++;						//read each line
+					
+					if (current_data <= 0) {	// Check data is positive
+						System.out.println("\n\tError: Data at file line " + (act_lines) + " is negative.");
+						System.exit(0);
+					}
+
+				}
+			} else {
+				System.out.println("\n\tError: The file is empty.");
+				System.exit(0);
+			}
+		} 
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		catch(NumberFormatException e) {
+			System.out.println("\n\tError: Data at file line " + (act_lines+1) + " is empty or non-integer.");
+			System.exit(0);
+		}
+		
+		if(num_lines == act_lines) {
+			System.out.println("\n\tThe file is in the correct format.\nSupposed lines: " + num_lines + "\nActual Lines: " + act_lines);
+		} else {
+			System.out.println("\n\tError: The file is not in the correct format." +
+				"\n\tSupposed lines: " + num_lines + 
+				"\n\tActual Lines: " + act_lines);
+		}
+	}
+
+
+}
--- project2.py
+++ project2.py
@@ -5,8 +5,9 @@ import sys, random, copy
 people = []
 
 class Clique:
-    def __init__(self, items):
+    def __init__(self, items, exclude):
         self.data = set(items)
+        self.exclude = set(exclude)
 
     def __str__(self):
         return str(self.data)
@@ -22,7 +23,8 @@ class Clique:
         r = range(0, len(people))
         random.shuffle(r)
         for p in r:
-            if p not in self.data and people[p].isFriendsWithAll(self.data):
+            if (p not in self.data and people[p].isFriendsWithAll(self.data) 
+                    and p not in self.exclude):
                 self.data.add(p)
 
     """
@@ -33,13 +35,13 @@ class Clique:
         items = list(self.data)
         start = random.randint(0, len(items) - 1)
         for p in items[start:]:
-            if not people[p].isFriendsWithAll(self.data):
+            if not people[p].isFriendsWithAll(self.data) or p in self.exclude:
                 self.data.discard(p)
 
         itemsr = items[:start]
         itemsr.reverse()
         for p in itemsr:
-            if not people[p].isFriendsWithAll(self.data):
+            if not people[p].isFriendsWithAll(self.data) or p in self.exclude:
                 self.data.discard(p)
 
 
@@ -49,7 +51,7 @@ class Clique:
     def mutate(self, n):
         candidates = []
         for p in range(0, len(people)):
-            if p not in self.data:
+            if p not in self.data and p not in self.exclude:
                 candidates += [(p, people[p].countFriendsWith(self.data))]
             
         candidates.sort(lambda a, b: a[1] - b[1])
@@ -61,7 +63,7 @@ class Clique:
 
     def breed(self, other):
         self.merge(other)
-        self.mutate(random.randint(0, 10))
+        self.mutate(random.randint(0, 5))
 
     def merge(self, other):
         self.makeMaximal()
@@ -110,10 +112,9 @@ def load(filename):
         people.append(Person(p, [int(f) for f in friendlist.split(',')]))
         p += 1
 
-if __name__ == "__main__":
-    load(sys.argv[1])
-    newpop = [Clique([p]) for p in random.sample(range(len(people)), 20)]
-    for gen in range(100):
+def GAmaxclique(exclude, gens):
+    newpop = [Clique([p], exclude) for p in random.sample(range(len(people)), 20)]
+    for gen in range(gens):
         pop = newpop
         pop.sort(lambda a, b: b.fitness() - a.fitness())
         print 'Generation %d: ' % gen + ' '.join(str(p.fitness()) for p in pop)
@@ -129,9 +130,40 @@ if __name__ == "__main__":
                 #print "as ", str(newc)
                 newpop.append(newc)
                 chosen.add((newc, newc2))
+    return pop[0]
 
-    print "Results: "
-    print ' '.join(str(p.fitness()) for p in pop[:3])
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: python project2.py <input file> [<# of generations>]"
 
+    gens = 20
+    if len(sys.argv) > 2:
+        gens = int(sys.argv[2])
 
+    load(sys.argv[1])
+
+    p1 = GAmaxclique([], gens)
+    p2 = GAmaxclique(p1.data, gens)
+    p3 = GAmaxclique(p1.data | p2.data, gens)
+
+    print p1
+    print p2
+    print p3
+    print
+    print (p1.data & p2.data)
+    print (p2.data & p3.data)
+    print (p1.data & p3.data)
+    print
+    print len(p1.data), len(p2.data), len(p3.data)
+    print "total: %d" % (len(p1.data) + len(p2.data) + len(p3.data))
+
+    if raw_input('Accept results? (y/n)').lower() == 'y':
+        d1 = open('Data1.dat', 'w')
+        d2 = open('Data2.dat', 'w')
+        d3 = open('Data3.dat', 'w')
+
+        for p, d in zip([p1.data, p2.data, p3.data], [d1, d2, d3]):
+            d.write('%d\n' % (len(p) + 1))
+            d.write(''.join(['%d\n' % person for person in p]))
+            d.close()
 
